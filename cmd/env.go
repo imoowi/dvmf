93326e0c6eb49bc039610f20cb7f101df399a010
simple yuan <simpleyuan@gmail.com>
2025-02-16 11:40:21 +0800

    Update env.go

--- cmd/env.go
+++ cmd/env.go
@@ -11,13 +11,8 @@ import (
 // envCmd represents the env command
 var envCmd = &cobra.Command{
 	Use:   "env",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Distribute env variables into multiple files.",
+	Long: `Distribute env variables into multiple files.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		env.Do(cfgFile)
 	},
